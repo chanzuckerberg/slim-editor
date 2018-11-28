5bbd864a2a598987dcbf296a3c3c435b50498720
fresh-prince-of-prosemirror <45044826+fresh-prince-of-prosemirror@users.noreply.github.com>
2018-11-28 11:46:34 -0800

    1/n set up custom render block

---
+++ src/editor_configuration/SlimEditorBlockRenderer.js
@@ -0,0 +1,51 @@
+import DocsBlockTypeToComponent from "./DocsBlockTypeToComponent.js";
+import tryGetEntityAtContentState from "../utils/tryGetEntityAtContentState";
+import React from "react";
+import { ContentBlock } from "draft-js";
+
+type Props = {
+  editorState: EditorState
+};
+
+function renderBlock(contentBlock: ContentBlock, blockProps: Props) {
+  let component;
+  let props;
+  let editable;
+  switch (contentBlock.getType()) {
+    case "atomic":
+      const contentState = blockProps.editorState.getCurrentContent();
+      const entityKey = contentBlock.getEntityAt(0);
+      // entity could be `null`.
+      // This happens while pasting HTML from external sources and we failed
+      // to parse its data.
+      const entity = entityKey
+        ? tryGetEntityAtContentState(contentState, entityKey)
+        : null;
+      if (entity) {
+        component = DocsBlockTypeToComponent.getComponent(entity.getType());
+        editable = false;
+        props = {
+          ...blockProps,
+          entity,
+          entityKey
+        };
+      }
+      break;
+
+    default:
+      return null;
+  }
+
+  if (!component) {
+    return null;
+  }
+
+  return {
+    component,
+    editable,
+    props
+  };
+}
+module.exports = {
+  renderBlock
+};
