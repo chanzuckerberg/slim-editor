f29c23194c2354ac7616319caf4fd9de6c938842
fresh-prince-of-prosemirror <45044826+fresh-prince-of-prosemirror@users.noreply.github.com>
2018-12-04 09:38:35 -0800

    create convertDraftEditorStateToHTML

--- src/components/slimEditor.js
+++ src/components/slimEditor.js
@@ -1,48 +1,25 @@
 // @flow
-import { ContentBlock, Editor } from "draft-js";
+import { ContentBlock, Editor, EditorState } from "draft-js";
 import React from "react";
-import ReactDOM from "react-dom";
 import SlimEditorBlockRenderer from "../editor_configuration/SlimEditorBlockRenderer.js";
-
-import nullthrows from "nullthrows";
-import { parseLists } from "../utils/sanitizeLists.js";
-
-import type { SerializedListType } from "../utils/sanitizeLists.js";
 
 const DEFAULT_EDITOR_WIDTH = 696;
 
 type Props = {|
-  editorState: Object,
-  updateState: Function
+  editorState: EditorState,
 |};
+
 export default class SlimEditor extends React.PureComponent<any, any, any> {
-  constructor(props: Props) {
-    super(props);
-    this.state = { html: "" };
-  }
+  props: Props;
 
   render() {
-    return <textarea value={this.state.html} />;
-  }
-
-  componentDidUpdate = (prevProps: Props) => {
-    if (prevProps.editorState !== this.props.editorState) {
-      const el = document.createElement("div");
-
-      const editor = (
-        <Editor
-          blockRendererFn={this._renderBlock}
-          editorState={this.props.editorState}
-          onChange={this.props.updateState}
+    return (
+      <Editor
+        blockRendererFn={this._renderBlock}
+        editorState={this.props.editorState}
         />
-      );
-
-      ReactDOM.render(editor, el);
-      const html = this.simplifyLists(el);
-      this.setState({ html });
-      ReactDOM.unmountComponentAtNode(el);
-    }
-  };
+    );
+  }
 
   _renderBlock = (contentBlock: ContentBlock): ?Object => {
     const { editorState } = this.props;
@@ -51,32 +28,5 @@ export default class SlimEditor extends React.PureComponent<any, any, any> {
       editorWidth: DEFAULT_EDITOR_WIDTH
     };
     return SlimEditorBlockRenderer.renderBlock(contentBlock, blockProps);
-  };
-
-  simplifyLists = (el: HTMLElement): ?string => {
-    const editorClone: HTMLElement = el.cloneNode(true);
-    parseLists(editorClone, "ul").forEach(list => {
-      this._replaceOldListWithProperlyNestedList(list, editorClone);
-    });
-    parseLists(editorClone, "ol").forEach(list => {
-      this._replaceOldListWithProperlyNestedList(list, editorClone);
-    });
-    return editorClone.innerHTML;
-  };
-
-  _replaceOldListWithProperlyNestedList = (
-    list: SerializedListType,
-    editorNode: HTMLElement
-  ) => {
-    const originalListElement = editorNode.querySelector(
-      `.public-DraftStyleDefault-${list.tag}[data-offset-key="${list.key}"]`
-    );
-    if (originalListElement) {
-      const parentNode = nullthrows(originalListElement.parentNode);
-      list.nodes.forEach(node =>
-        parentNode.insertBefore(node, originalListElement)
-      );
-      parentNode.removeChild(originalListElement);
-    }
   };
 }
